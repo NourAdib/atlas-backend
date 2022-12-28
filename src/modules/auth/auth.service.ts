098061e176c4427f18bcd6d061988ca270facc36
ZaydHW <za2011@hw.ac.uk>
2022-12-28 21:07:21 +0400

    updated auth.service.ts

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,10 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { EncryptionService } from 'src/common/services/encryption.service';
+import { Role } from 'src/constants/role.enum';
+import { Repository } from 'typeorm';
+import { User } from '../user/user.entity';
 import { UserService } from '../user/user.service';
+import { SignUpUserDto } from './dto/user-signup.dto';
 
 @Injectable()
 export class AuthService {
+  private usersRepository: Repository<User>;
   constructor(private usersService: UserService, private jwtService: JwtService) {}
 
   /**
@@ -34,5 +39,25 @@ export class AuthService {
     return {
       access_token: this.jwtService.sign(payload)
     };
+  }
+  /**
+   * Creates a new user object and saves it in the database
+   * @param user the user object that contains the user information
+   * @returns the newly created user
+   */
+  createAdmin(user: SignUpUserDto): Promise<User> {
+    const newUser = new User();
+    newUser.firstName = user.firstName;
+    newUser.lastName = user.lastName;
+    newUser.email = user.email;
+    newUser.username = user.username;
+    newUser.password = user.password;
+    newUser.phoneNumber = user.phoneNumber;
+    newUser.address = user.address;
+    newUser.dateOfBirth = new Date(user.dateOfBirth);
+
+    //The user is an admin
+    newUser.role = Role.Admin;
+    return this.usersRepository.save(newUser);
   }
 }
