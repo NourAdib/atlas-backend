7a5564d5799a3b26680e89249e3e385c6d6ccb14
Nemr Aslam <78597927+Nimmbo02@users.noreply.github.com>
2023-03-29 22:01:42 +0400

    commented

--- src/decorators/subscription.decorator.ts
+++ src/decorators/subscription.decorator.ts
@@ -1,6 +1,12 @@
 import { SetMetadata } from '@nestjs/common';
 import { SubscriptionPlan } from '../constants/subscription-plan.enum';
-
+/**
+ * SUBSCRIPTION_PLANS_KEY is the key used to store the subscription plans in the metadata
+ */
 export const SUBSCRIPTION_PLANS_KEY = 'subscription_plans';
+/**
+ * @param subscription_plans the subscription plans to be stored in the metadata
+ * @returns checks if the subscription plans are a part of the SubscriptionPlan enum
+ */
 export const SubscriptionPlans = (...subscription_plans: SubscriptionPlan[]) =>
   SetMetadata(SUBSCRIPTION_PLANS_KEY, subscription_plans);
