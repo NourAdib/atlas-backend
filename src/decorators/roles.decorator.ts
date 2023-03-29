b74d8e9f6cded81152171597df56fbd731e3723d
Nemr Aslam <78597927+Nimmbo02@users.noreply.github.com>
2023-03-29 22:01:16 +0400

    commented

--- src/decorators/roles.decorator.ts
+++ src/decorators/roles.decorator.ts
@@ -1,5 +1,11 @@
 import { SetMetadata } from '@nestjs/common';
 import { Role } from '../constants/role.enum';
-
+/**
+ * ROLES_KEY is the key used to store the roles in the metadata
+ */
 export const ROLES_KEY = 'roles';
+/**
+ * @param roles the roles to be stored in the metadata
+ * @returns Checks if the roles are a part of the Role enum
+ */
 export const Roles = (...roles: Role[]) => SetMetadata(ROLES_KEY, roles);
